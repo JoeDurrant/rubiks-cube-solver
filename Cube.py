6876ce076fa90e598c1112d5cfbf7e8bdf227318
JoeDurrant <josephwdurrant@gmail.com>
2020-01-18 23:39:34 +0000

    Start implementation of front layer rotation method

--- Cube.py
+++ Cube.py
@@ -11,6 +11,22 @@ class Cube:
         return
     
     def rotate_front(self, degree):
+        if degree == 180:
+            temp_side_top = self.sides[0][2][::-1]
+            temp_side_left = []
+            temp_side_left.append(self.sides[1][0][2])
+            temp_side_left.append(self.sides[1][1][2])
+            temp_side_left.append(self.sides[1][2][2])
+            temp_side_left = temp_side_left[::-1]
+
+            print(temp_side_top)
+            print(temp_side_left)
+
+            for i in range(3):
+                #Assign side values to appropriate colours
+
+            self.sides[0][2] = self.sides[4][0][::-1]
+        self.print_cube()
         return
     def rotate_back(self, degree):
         return
@@ -28,3 +44,25 @@ class Cube:
         return
     def rotate_standing(self, degree):
         return
+    def print_cube(self):
+        print("        " + " ".join(self.sides[0][0]))
+        print("        " + " ".join(self.sides[0][1]))
+        print("        " + " ".join(self.sides[0][2]))
+
+        print()
+
+        print(" ".join(self.sides[1][0]) + "   " + " ".join(self.sides[2][0]) + "   " + " ".join(self.sides[3][0]))
+        print(" ".join(self.sides[1][1]) + "   " + " ".join(self.sides[2][1]) + "   " + " ".join(self.sides[3][1]))
+        print(" ".join(self.sides[1][2]) + "   " + " ".join(self.sides[2][2]) + "   " + " ".join(self.sides[3][2]))
+
+        print()
+
+        print("        " + " ".join(self.sides[4][0]))
+        print("        " + " ".join(self.sides[4][1]))
+        print("        " + " ".join(self.sides[4][2]))
+
+        print()
+
+        print("        " + " ".join(self.sides[5][0]))
+        print("        " + " ".join(self.sides[5][1]))
+        print("        " + " ".join(self.sides[5][2]))
--- solve-cube.py
+++ solve-cube.py
@@ -3,16 +3,16 @@ from Cube import Cube
 def solve_cube(state):
     #TODO: Logic of cube solving
     cube = Cube(state)
-    
+
     return
 
 cube_sides = [
-        [['W','W','W'], ['W','W','W'], ['W','W','W']],
-        [['O','O','O'], ['O','O','O'], ['O','O','O']],
-        [['R','R','R'], ['R','R','R'], ['R','R','R']],
-        [['B','B','B'], ['B','B','B'], ['B','B','B']],
-        [['Y','Y','Y'], ['Y','Y','Y'], ['Y','Y','Y']],
-        [['G','G','G'], ['G','G','G'], ['G','G','G']],
+        [['W','W','W'], ['W','W','W'], ['W','W','W']],#top
+        [['G','O','G'], ['G','O','G'], ['G','Y','Y']],#left
+        [['R','R','B'], ['Y','G','O'], ['O','O','Y']],#front
+        [['O','B','R'], ['Y','R','O'], ['R','R','B']],#right
+        [['G','B','B'], ['B','Y','Y'], ['R','R','Y']],#bottom
+        [['Y','B','O'], ['R','B','G'], ['O','G','B']],#back
         ]
 
 print("        " + " ".join(cube_sides[0][0]))
@@ -36,3 +36,7 @@ print()
 print("        " + " ".join(cube_sides[5][0]))
 print("        " + " ".join(cube_sides[5][1]))
 print("        " + " ".join(cube_sides[5][2]))
+
+cube = Cube(cube_sides)
+
+cube.rotate_front(180)
